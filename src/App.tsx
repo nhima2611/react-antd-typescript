2ffadc5ac01adc2694e2ebda157da72621c1b7c2
nnhi3113 <99382110+nhima2611@users.noreply.github.com>
2023-10-10 21:37:17 +0700

    main push

--- src/App.tsx
+++ src/App.tsx
@@ -86,6 +86,7 @@ const App: React.FC = () => {
               }}
               tip={<LocaleFormatter id="gloabal.tips.loading" />}
             ></Spin>
+            <p>branch main</p>
             <RenderRouter />
           </Suspense>
         </HistoryRouter>
